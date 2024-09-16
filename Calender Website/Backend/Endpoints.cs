1513eb8b5dae6487d2451d00ff9fa6a12c8a5705
Anton Ishchyk <anton.ishchyk@gmail.com>
2024-09-16 11:33:14 +0200

    start on DB, endpoints

--- Calender Website/Backend/Endpoints.cs
+++ Calender Website/Backend/Endpoints.cs
@@ -1,11 +1,33 @@
-namespace Backend
+using Backend;
+
+namespace EndPointManager
 {
     public class EndPointManager
     {
         private WebApplication App { get; set; }
+
+        MemoryDB DB;
+
         public EndPointManager(WebApplication app)
         {
             App = app;
+            DB = new MemoryDB();
+
+            app.MapPost("/Register/", Register);
+            app.MapPost("/Login/", Login);
+
+
+        }
+
+        public IResult Register()
+        {
+            return Results.Ok("To Implement");
+        }
+
+        public IResult Login()
+        {
+            // after loggin in - create a new session
+            return Results.Ok("To Implement");
         }
     }
 }
