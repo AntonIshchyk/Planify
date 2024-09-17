3ce7e8f4c6a73c44651e08d818978043f4c48f75
Mr. L NL <itchy8mekidsl@gmail.com>
2024-09-17 09:55:36 +0200

    controllers&services relocation

--- Calender Website/Backend/Endpoints.cs
+++ Calender Website/Backend/Endpoints.cs
@@ -5,67 +5,6 @@ namespace Backend
         private WebApplication App { get; set; }
 
         MemoryDB DB;
-
-        public EndPointManager(WebApplication app)
-        {
-            App = app;
-            DB = new MemoryDB();
-
-            app.MapPost("/Register/", Register);
-            app.MapPost("/Login/", Login);
-            app.MapPost("/Logout/", Logout);
-
-
-        }
-
-        
-        public IResult Register(Admin admin)
-        {
-            var existingAdmin = MemoryDB.Admins.FirstOrDefault(a => a.Username == admin.Username && a.Password == admin.Password);
-            if (existingAdmin is not null)
-            {
-                return Results.BadRequest("This Admin already exists");
-            }
-            MemoryDB.Admins.Add(admin);
-            return Results.Ok("Admin registered");
-        }
-
-        public IResult Login(Admin admin)
-        {
-            var existingAdmin = MemoryDB.Admins.FirstOrDefault(a => a.Username == admin.Username && a.Password == admin.Password);
-            if (existingAdmin is null)
-            {
-                return Results.BadRequest("Admin not found");
-            }
-            else
-            {
-                if (existingAdmin.LoggedIn)
-                {
-                    return Results.BadRequest("Admin is already online");
-                }
-                existingAdmin.LastLogIn = DateTime.Now;
-                existingAdmin.LoggedIn = true;
-                return Results.Ok($"Welcome {existingAdmin.Username}!");
-            }
-        }
-
-        public IResult Logout(Admin admin)
-        {
-            var existingAdmin = MemoryDB.Admins.FirstOrDefault(a => a.Username == admin.Username && a.Password == admin.Password);
-            if (existingAdmin is null)
-            {
-                return Results.BadRequest("Admin not found");
-            }
-            else
-            {
-                if (existingAdmin.LoggedIn)
-                {
-                    existingAdmin.LastLogOut = DateTime.Now;
-                    existingAdmin.LoggedIn = false;
-                    return Results.Ok($"See you later {existingAdmin.Username}!");
-                }
-                return Results.BadRequest("Admin is already offline");
-            }
-        }
+    
     }
 }
