fcccdec929de36a6bd70f364518cd97105960e4c
Daan van der Meer <daanvdmeer11@gmail.com>
2024-09-17 12:46:42 +0200

    formated controllers and services

--- Calender Website/Backend/EventControllers.cs
+++ Calender Website/Backend/EventControllers.cs
@@ -1,29 +1,22 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 
 [Microsoft.AspNetCore.Components.Route("api/events")]
-public class EventController : Controller{
-
+public class EventController : Controller
+{
     EventService eventService;
-    public EventController(EventService eventservice){
+    public EventController(EventService eventservice)
+    {
         eventService = eventservice;
     }
     [HttpGet()]
-    public async Task<IResult> GetEvent([FromQuery] Guid id){
-        return Results.Ok(GetEvent(id));
-    }
+    public async Task<IResult> GetEvent([FromQuery] Guid id) => Results.Ok(await GetEvent(id));
 
     [HttpPost]
-    public async Task<IResult> PostEvent([FromBody] Event e){
-        return await eventService.AppendEvent(e);
-    }
+    public async Task<IResult> PostEvent([FromBody] Event e) => await eventService.AppendEvent(e);
+
     [HttpPut]
-    public async Task<IResult> UpdateEvent([FromBody] Event e){
-        return await eventService.UpdateEvent(e);
-    }
+    public async Task<IResult> UpdateEvent([FromBody] Event e) => await eventService.UpdateEvent(e);
+
     [HttpDelete]
-    public async Task<IResult> DeleteEvent([FromQuery] Guid id){
-        return await eventService.DeleteEvent(id);
-    }
-    
+    public async Task<IResult> DeleteEvent([FromQuery] Guid id) => await eventService.DeleteEvent(id);
 }
