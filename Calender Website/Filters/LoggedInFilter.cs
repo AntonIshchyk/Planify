7fe12fcd9d905584d214f10a8d85d87f5ef0cd5c
Mr. L NL <itchy8mekidsl@gmail.com>
2024-09-25 14:56:22 +0200

    Filter & Login Sessions Fixed

--- Calender Website/Filters/LoggedInFilter.cs
+++ Calender Website/Filters/LoggedInFilter.cs
@@ -4,24 +4,16 @@ public class LoggedInFilter : Attribute, IAsyncActionFilter
 {
     public async Task OnActionExecutionAsync(ActionExecutingContext actioncontext, ActionExecutionDelegate next)
     {
-        var context = actioncontext.HttpContext;
-        SessionService SS = new();
-         var queryParams = context.Request.Query;
-
+       
         // Example: Get a specific query parameter by key (e.g., "id")
-        if(!queryParams.TryGetValue("idPerson", out var idValue)){
-            Console.WriteLine($"{context.Request.Path} was requested, but no id was given");
-            context.Response.StatusCode = 401;
+        if(!actioncontext.HttpContext.Session.TryGetValue("UserId", out var idValue)){
+            Console.WriteLine($"{actioncontext.HttpContext.Request.Path} was requested, but no id was given");
+            actioncontext.HttpContext.Response.StatusCode = 401;
             return;
         }
-         if(!Guid.TryParse(idValue, out var parsedId)){
-                Console.WriteLine($"{context.Request.Path} was requested, but no valid id was given");
-                context.Response.StatusCode = 401;
-                return;
-            }
-        if(SS.GetSessionByPersonId(parsedId) is null){
-            Console.WriteLine($"{context.Request.Path} was requested, but the user is not logged in!");
-            context.Response.StatusCode = 401;
+        if(idValue is null){
+            Console.WriteLine($"{actioncontext.HttpContext.Request.Path} was requested, but the user is not logged in!");
+            actioncontext.HttpContext.Response.StatusCode = 401;
             return;
         }
         await next();
