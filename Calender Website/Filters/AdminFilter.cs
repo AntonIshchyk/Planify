5fb8d86e0596e4ec920c1f95f3fc88eaf996083f
Mr. L NL <itchy8mekidsl@gmail.com>
2024-10-01 12:09:45 +0200

    Admins and Users are now separate

---
+++ Calender Website/Filters/AdminFilter.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class AdminFilter : Attribute, IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext actioncontext, ActionExecutionDelegate next)
+    {
+         var isAdmin = actioncontext.HttpContext.Session.GetInt32("IsAdmin");
+        // Example: Get a specific query parameter by key (e.g., "id")
+        if (!(isAdmin.HasValue && isAdmin.Value == 1))
+        {
+            Console.WriteLine($"{actioncontext.HttpContext.Request.Path} was requested, but no id was given");
+            actioncontext.HttpContext.Response.StatusCode = 401;
+            return;
+        }
+        await next.Invoke();
+    }
+}
