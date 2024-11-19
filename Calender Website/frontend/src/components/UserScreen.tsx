14fe969aceef136d0bfa154623671a55b63cba32
Mr. L NL <itchy8mekidsl@gmail.com>
2024-11-19 13:13:50 +0100

    Logout & Menu Bar

---
+++ Calender Website/frontend/src/components/UserScreen.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+const UserScreen : React.FC = () => {
+    return (
+        <div>
+            <div className='narrator'>Welcome User!</div>
+        </div>
+    );
+};
+
+export default UserScreen;
