f86d8870382a53034df4b4cb8d22f4afc3f1e58b
Anton Ishchyk <anton.ishchyk@gmail.com>
2024-12-03 10:21:53 +0100

    optimization, get many user at once, not 1 by one

--- Calender Website/frontend/src/components/Friends/Friends.tsx
+++ Calender Website/frontend/src/components/Friends/Friends.tsx
@@ -17,26 +17,31 @@ export class Friends extends React.Component<{}, FriendsState>
     }
 
     fetchFriends = async () => {
-        try {
+        try 
+        {
             const response = await axios.get(
                 'http://localhost:3000/Calender-Website/friends',
                 { withCredentials: true }
             );
 
+            const friendIds: string[] = response.data;
+
             // Fetch details for each friend
-            const friendDetails = await Promise.all(
-                response.data.map((id: string) =>
-                    axios.get(`http://localhost:3000/Calender-Website/get-user-by-id?Id=${id}`, { withCredentials: true })
-                )
+            const friendsResponse = await axios.get(
+                `http://localhost:3000/Calender-Website/get-many-users-by-id?${friendIds.map(id =>`Ids=${id}`).join('&')}`,
+                { withCredentials: true }
             );
 
-            const friends = friendDetails.map(friendResponse => friendResponse.data);
-            this.setState(this.state.updateFriends(friends));
-            
-        } catch (error) {
-            if (axios.isAxiosError(error) && error.response) {
+            this.setState(this.state.updateFriends(friendsResponse.data));
+        } 
+        catch (error) 
+        {
+            if (axios.isAxiosError(error) && error.response) 
+            {
                 toast.error(error.response.data);
-            } else {
+            } 
+            else 
+            {
                 toast.error('An error occurred. Please try again.');
             }
         }
@@ -55,7 +60,7 @@ export class Friends extends React.Component<{}, FriendsState>
                         <h3>{friend.lastName}</h3>
                         <h3>{friend.email}</h3>
                         <h3>Recurring Days {friend.recurringDays}</h3>
-                        We could also show friends of our friend
+                        <h3>We could also show friends of our friend</h3>
                         <br />
                     </div>
                 ))
