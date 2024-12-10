9371cbfaa23f5b1519896bab7d38afef114a1121
Anton Ishchyk <anton.ishchyk@gmail.com>
2024-12-10 18:22:26 +0100

    StateUpdater<EventListState>

--- Calender Website/frontend/src/components/Events/EventList.state.ts
+++ Calender Website/frontend/src/components/Events/EventList.state.ts
@@ -1,3 +1,5 @@
+import {StateUpdater, createStateUpdater} from '../Updater/Updater';
+
 interface Event {
     id: string;
     title: string;
@@ -11,14 +13,12 @@ interface Event {
 
 export type EventListState = {
     events : Event[],
-    updateEvents : (events : Event[]) => (state : EventListState) => EventListState,
     attending : string[],
-    updateAttending : (attending : string[]) => (state : EventListState) => EventListState,
+    updateField: StateUpdater<EventListState>;
     }
 
 export const initEventListState = {
     events : [],
-    updateEvents : (events : Event[]) => (state:EventListState) => ({...state, events : events}),
     attending : [],
-    updateAttending : (attending : string[]) => (state:EventListState) => ({...state, attending : attending})
+    updateField: createStateUpdater<EventListState>()
 }
