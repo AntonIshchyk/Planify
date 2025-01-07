93784a132634d0ac6240c178f557eb7aaa6b095a
Daan van der Meer <daanvdmeer11@gmail.com>
2025-01-07 13:33:22 +0100

    merge

--- Calender Website/frontend/src/App.tsx
+++ Calender Website/frontend/src/App.tsx
@@ -117,18 +117,6 @@ export class App extends Component<{}, AppState> {
     return (
       <BrowserRouter>
         <div className="App">
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
 
           <MenuBar
           
