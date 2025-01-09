4003c8d834b3f5de3e7484af78d51e6bccb2e677
Anton Ishchyk <anton.ishchyk@gmail.com>
2025-01-09 18:09:01 +0100

    register/login

--- Calender Website/frontend/src/components/Register/Register.tsx
+++ Calender Website/frontend/src/components/Register/Register.tsx
@@ -3,92 +3,112 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 import { initRegisterState, RegisterState } from '../Register/Register.state';
 import apiClient from '../../ApiClient';
+import './Register.css'; // Import the CSS file
 
 interface RegisterProps {
-    onBacktoMenuClick : () => void;
+  onBacktoMenuClick: () => void;
 }
-export class Login extends React.Component<RegisterProps, RegisterState> {
-    constructor(props : RegisterProps){
-        super(props);
-        this.state = initRegisterState;
-        
+
+export class Register extends React.Component<RegisterProps, RegisterState> {
+  constructor(props: RegisterProps) {
+    super(props);
+    this.state = initRegisterState;
+  }
+
+  handleRegister = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await apiClient.post(
+        'http://localhost:3000/Calender-Website/register',
+        {
+          Email: this.state.email,
+          FirstName: this.state.firstName,
+          LastName: this.state.lastName,
+          Password: this.state.password,
+        },
+        { withCredentials: true }
+      );
+      localStorage.setItem('message', response.data);
+      window.location.reload();
+      window.dispatchEvent(new Event('storageUpdated'));
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) {
+        toast.error(error.response.data);
+      } else {
+        toast.error('There is an error');
+      }
     }
-    handleRegister = async (event : React.FormEvent) => {
-        event.preventDefault();
-        try{
-                const response = await apiClient.post(
-                'http://localhost:3000/Calender-Website/register',
-                {  "Email" : this.state.email,
-                    "FirstName" : this.state.firstName, 
-                 "LastName" : this.state.lastName,
-                 "Password" : this.state.password },
-                 {withCredentials: true}
-                )
-            localStorage.setItem('message', response.data);
-            window.location.reload();
-            window.dispatchEvent(new Event('storageUpdated'));
-        }
-        catch(error){
-            if (axios.isAxiosError(error) && error.response) {
-                toast.error(error.response.data);
-            } else {
-                toast.error("There is an error");
-            }
-        }
-    };
+  };
 
-    render() {
-        return(
-        <div>
-            <h2>Register</h2>
-            <form onSubmit={this.handleRegister}>
-                <label>
-                    Email:
-                    <input 
-                    type="email"
-                    value={this.state.email}
-                    onChange={(e) => this.setState(this.state.updateField("email", e.currentTarget.value))}
-                    required />
-                </label>
-                <br />
-                <label>
-                    First Name:
-                    <input 
-                    type="username"
-                    value={this.state.firstName}
-                        onChange={(e) => this.setState(this.state.updateField("firstName", e.currentTarget.value))}
-                    required />
-                </label>
-                <br />
-                <label>
-                    Last Name:
-                    <input 
-                    type="username"
-                    value={this.state.lastName}
-                        onChange={(e) => this.setState(this.state.updateField("lastName", e.currentTarget.value))}
-                    required />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input 
-                    type="password" 
-                    value={this.state.password} 
-                    onChange = {(e) => this.setState(this.state.updateField("password", e.currentTarget.value))}
-                    required/>
-                </label>
-                <br />
-                <button type="submit">Register</button>
-            </form>
+  render() {
+    return (
+      <div className="register-container">
+        <h2>Register</h2>
+        <form onSubmit={this.handleRegister}>
+          <label>
+            Email:
+            <input
+              type="email"
+              value={this.state.email}
+              onChange={(e) =>
+                this.setState(this.state.updateField('email', e.currentTarget.value))
+              }
+              required
+            />
+          </label>
+          <br />
+          <label>
+            First Name:
+            <input
+              type="text"
+              value={this.state.firstName}
+              onChange={(e) =>
+                this.setState(this.state.updateField('firstName', e.currentTarget.value))
+              }
+              required
+            />
+          </label>
+          <br />
+          <label>
+            Last Name:
+            <input
+              type="text"
+              value={this.state.lastName}
+              onChange={(e) =>
+                this.setState(this.state.updateField('lastName', e.currentTarget.value))
+              }
+              required
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={(e) =>
+                this.setState(this.state.updateField('password', e.currentTarget.value))
+              }
+              required
+            />
+          </label>
+          <br />
+          <button type="submit">Register</button>
+        </form>
 
-            <form onSubmit={(event) => {
-                event.preventDefault();
-                this.props.onBacktoMenuClick();
-            }}>
-            <button type="submit">Back to Menu</button>
-            </form>
-        </div>
-)};
-};
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            this.props.onBacktoMenuClick();
+          }}
+        >
+          <button className="back-btn" type="submit">
+            Back to Menu
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
-export default Login;   
+export default Register;
